f083da41269c42694a0db0fdd859f980f2cfae2d
Kaivinkone <142224116+Kaivinkone@users.noreply.github.com>
2023-09-05 11:54:18 +0300

    Initial commit

---
+++ src/Guessing_name.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Guessing_name {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		String arvaus = "";
+		String vastaus = "";
+		int laskuri = 0;
+		
+		System.out.println("Tervetuloa pelaamaan.");
+		
+		do {
+		System.out.println("Arvaa nimi.");
+		arvaus = in.nextLine();
+		laskuri++;
+				
+		if (arvaus.equals("Pentti")) {
+			System.out.println("Onneksi olkoon, voitit!");
+			break;
+		}
+		
+		else if (!arvaus.equals("Pentti")) {
+			System.out.println("Et voittanut tällä kertaa.");
+			System.out.println("Paina enteriä pelataksesi uudestaan. Lopettaaksesi pelin kirjoita lopeta");
+			vastaus = in.nextLine();
+		}
+			
+		}while (!vastaus.equals("lopeta")); 
+		System.out.println("Arvastit " + laskuri + " kertaa. Kiitos pelaamisesta!");
+		
+
+		}
+
+	}
