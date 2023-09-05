49eecf1b64d46c4bdc8ddf7e2a28597fca131775
Kaivinkone <142224116+Kaivinkone@users.noreply.github.com>
2023-09-05 12:00:44 +0300

    Kommentit

--- src/Guessing_name.java
+++ src/Guessing_name.java
@@ -12,22 +12,25 @@ public class Guessing_name {
 		
 		System.out.println("Tervetuloa pelaamaan.");
 		
-		do {
+		// looppirakenteen lisääminen
+		do { 
 		System.out.println("Arvaa nimi.");
 		arvaus = in.nextLine();
 		laskuri++;
 				
+		// Pentti vastaus voittaa ja katkaisee loopin
 		if (arvaus.equals("Pentti")) {
 			System.out.println("Onneksi olkoon, voitit!");
 			break;
 		}
 		
+		// Ilman Penttiä palaute ja uudelleen kysymys
 		else if (!arvaus.equals("Pentti")) {
 			System.out.println("Et voittanut tällä kertaa.");
 			System.out.println("Paina enteriä pelataksesi uudestaan. Lopettaaksesi pelin kirjoita lopeta");
 			vastaus = in.nextLine();
 		}
-			
+		// Jatketaan looppia niin kauan kuin arvaus ei oo Pentti	
 		}while (!vastaus.equals("lopeta")); 
 		System.out.println("Arvastit " + laskuri + " kertaa. Kiitos pelaamisesta!");
 		
